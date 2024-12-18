628fad130e3a3e9d51228689abc871bf50e11830
frontDeveloper <gurbansahadowkemal01@gmail.com>
2024-12-18 15:03:39 +0300

    подлкючил шрифты

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+$('.form').on('submit', function (e) {
+  e.preventDefault();
+  const formData = $(this).serializeArray();
+  formData.map(item => {
+    console.log(item);
+  });
+});
