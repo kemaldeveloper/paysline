fe7317fcb8b5230ab22e7b145dd8942161a3a8c1
frontDeveloper <gurbansahadowkemal01@gmail.com>
2024-12-28 14:58:08 +0300

    сделал отправку форм авторизации

--- app/js/modules/formSubmit.js
+++ app/js/modules/formSubmit.js
@@ -1,9 +1,21 @@
 export const formSubmit = () => {
-  $('#signIn').on('submit', function (e) {
+  const authMessage = $('#authMessage');
+
+  $('.authorization__form').on('submit', function (e) {
     e.preventDefault();
 
     const formData = $(this).serialize();
 
+    const isSuccess = true;
+    const message = isSuccess ? 'Спасибо, ваши данные отправлены!' : 'Неверный email или пароль. Попробуйте еще раз.';
+
+    authMessage.text(message);
+    authMessage.removeClass('d-none', 'alert-success', 'alert-danger');
+    authMessage.addClass(isSuccess ? 'alert-success' : 'alert-danger');
+
     console.log(formData);
+    if (isSuccess) {
+      this.reset();
+    }
   });
 };
